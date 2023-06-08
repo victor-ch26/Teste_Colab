4f0e815adde58c6d5286f817aeeb53798a2274c7
victor-ch26 <130512810+victor-ch26@users.noreply.github.com>
2023-06-08 00:30:46 -0300

    Criado usando o Colaboratory

---
+++ Teste_Github.py
@@ -0,0 +1,42 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "toc_visible": true,
+      "authorship_tag": "ABX9TyOocW4yP4GioGX+DtmJ2seJ",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/victor-ch26/Teste_Colab/blob/main/Teste_Github.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "A5YHLd0N533c"
+      },
+      "outputs": [],
+      "source": [
+        "print(\"Olá Mundo\")"
+      ]
+    }
+  ]
+}
